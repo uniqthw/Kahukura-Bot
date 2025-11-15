5ad3e5767214b6af86fb4de07d0b663cace7c1fe
Carolina Mitchell <me@carolinamitchell.nz>
2025-11-15 18:55:38 +1300

    Fix massApplyUnverified

--- scripts/massApplyUnverified.ts
+++ scripts/massApplyUnverified.ts
@@ -13,7 +13,7 @@ const ROLE_ID = settings.discord.rolesID.unverified;   // Role to give
 const GUILD_ID = settings.discord.guildID; // Server where to add role
 
 client.once("ready", async () => {
-    console.log(`Logged in as ${client.user.tag}`);
+    console.log(`Logged in as ${client.user?.tag}`);
 
     try {
         const guild = await client.guilds.fetch(GUILD_ID);
@@ -24,28 +24,28 @@ client.once("ready", async () => {
             console.error("Role not found!");
             process.exit(1);
         }
+
+        console.log(`Assigning role to ${guild.members.cache.size} members...`);
+
+        for (const [id, member] of guild.members.cache) {
+            if (!member.roles.cache.has(ROLE_ID)) {
+                try {
+                    await member.roles.add(role);
+                    console.log(`Added role to: ${member.user.tag}`);
+                    await new Promise(res => setTimeout(res, 500)); // 0.5s delay for safety
+                } catch (err) {
+                    console.log(`Failed for ${member.user.tag}:`, err);
+                }
+            }
+        }
+
+        console.log("Done!");
+        client.destroy();
+        process.exit(0);
     } catch (error) {
         console.error("Failed to fetch guild or members:", error);
         process.exit(1);
     }
-
-    console.log(`Assigning role to ${guild.members.cache.size} members...`);
-
-    for (const [id, member] of guild.members.cache) {
-        if (!member.roles.cache.has(ROLE_ID)) {
-            try {
-                await member.roles.add(role);
-                console.log(`Added role to: ${member.user.tag}`);
-                await new Promise(res => setTimeout(res, 500)); // 0.5s delay for safety
-            } catch (err) {
-                console.log(`Failed for ${member.user.tag}:`, err);
-            }
-        }
-    }
-
-    console.log("Done!");
-    client.destroy();
-    process.exit(0);
 });
 
 client.login(TOKEN).catch(error => {
